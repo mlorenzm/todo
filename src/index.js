ac625ffad889e9dbc865dba6f51dbcbe92073efc
mlorenzm <mlorenzomohamed@gmail.com>
2023-05-17 16:39:45 +0100

    add overflow to main container

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import { createTaskItemContainer, renderTask } from './modules/dom'
 
 const item = CreateItem('Start with this todo app', 'desc', '1', '0', 'odin')
 const item2 = CreateItem('Continue with this app', 'desc', '1', '0', 'odin')
-const array = [item, item2]
-console.log(array)
 
-for (let a of array) {
+const taskArray = [
+  item2,
+  item2,
+  item2,
+  item2,
+  item2,
+  item2,
+  item2,
+  item2,
+  item2,
+  item2,
+  item2,
+  item2,
+]
+
+for (let a of taskArray) {
   renderTask(a)
 }
